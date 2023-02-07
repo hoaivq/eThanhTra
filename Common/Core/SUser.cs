a4abec5bbf21f6e236c9202ea31a15212d54e871
hoaivq <admin@HOAIVQ>
2023-02-07 16:12:29 +0700

    a

---
+++ Common/Core/SUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common
+{
+    // Hoaivq
+    public class SUser
+    {
+        public string UserName { get; set; }
+
+        private string Password { get; set; }
+
+        public int UserType { get; set; }
+
+        public string HoVaTen { get; set; }
+
+        public string Email { get; set; }
+
+        public string SoDienThoai { get; set; }
+
+        public string MaCQT { get; set; }
+
+        public bool IsEnable { get; set; }
+
+    }
+
+}
