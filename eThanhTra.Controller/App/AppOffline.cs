8c9fb4af58cd4c15c15fd2bc15a9249b6a1573bd
hoaivq <admin@HOAIVQ>
2023-02-16 09:48:54 +0700

    hoaivq

---
+++ eThanhTra.Controller/App/AppOffline.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using eThanhTra.Api.Controllers;
+using eThanhTra.Network.System;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eThanhTra.Controller.App
+{
+    public class AppOffline : IAppNetwork
+    {
+        AppController controller = new AppController();
+        public async Task<MsgResult<DataTable>> CallSP(CallSPDto callSPDto)
+        {
+            return await controller.CallSP(callSPDto);
+        }
+    }
+}
